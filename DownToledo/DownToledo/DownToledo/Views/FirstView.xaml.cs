c126b66b73b50f2e84be2081cb8f543be86af910
d.rivera <d.rivera@interlatin.com>
2018-04-23 13:54:54 +0200

    Principio app

---
+++ DownToledo/DownToledo/DownToledo/Views/FirstView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DownToledo.Views
+{
+	public partial class FirstView : ContentPage
+	{
+		public FirstView()
+		{
+			InitializeComponent();
+            ima.Source = ImageSource.FromResource("DownToledo.img.Logo.png",Assembly.GetExecutingAssembly());
+            loguin.Clicked += loguearse;
+            register.Clicked += registrarse;
+		}
+
+        private async void registrarse(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new RegisterPage());
+        }
+
+        private void loguearse(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
